65fbfe10366f550c688a61acd1a0d9ebf7338bb5
TWICEMOMO <rlaehdwn3331@gmail.com>
2018-04-10 23:02:49 +0900

    유저가원하는결과전송

--- app.js
+++ app.js
@@ -13,7 +13,8 @@ let jd;
 let wurl;
 let answer;
 let ans;
-
+let whe;
+const arr=[];
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: true
@@ -32,49 +33,65 @@ app.post('/message', function(req, res) {
     type: req.body.type,
     content: req.body.content
   };
+  console.log(_obj.content);
+  if(_obj.content=='독도') arr[4]='독도는 대한민국 땅입니다.\n';
+  else arr[4]=_obj.content+'를 찾을 수 없습니다\n';
   var city = urlencode(_obj.content);
   var url = 'https://maps.google.com/maps/api/geocode/json?address='+city+'&key=APIkey';
   A(url)
-  .then((data)=> B(data.result))
-  .then((data)=> {
-    console.log(data);
+  .then(data=> B(data.result))
+  .then(data=> {
     answer = {
       "message": {
         "text": ans
       }
     }
-    console.log('온도에요'+ans);
-    res.send(answer);
-  });
-  return;
+    res.send(answer);})
+    .catch(function(){
+      answer = {
+        "message": {
+          "text": arr[4]+'1. 서울강서\n2. 청주서원구\n3. 청주시서원구\n4. 단양군 도전리\n5. 안산고잔동\n등 형식으로 입력주세요.\n입력이 틀릴시 원하는 정보를 얻지 못할 수 있습니다.\n추가기능 or 에러발생시\n상담원톡해주세요~!\n※해외날씨버그로인해중단'
+        }
+      }
+      res.send(answer);
+    });
 });
 
-let A = (url) => new Promise((resolve)=>{
+let A = (url) => new Promise((resolve, reject)=>{
   console.log('A Executed');
   request({
     url: url,
     method: 'GET'
   }, function(error, response, body) {
     console.log('error : ', error);
-    console.log(url);
     jd = JSON.parse(response.body);
-    console.log(jd);
-    var whe = jd.results[0].geometry.location;
-    console.log('뀨뀨'+whe);
-    console.log('xy좌표');
-    calc.mapq(whe.lat,whe.lng);
-    var Xpos = calc.returnX();
-    var Ypos = calc.returnY();
-    wurl = 'http://www.kma.go.kr/wid/queryDFS.jsp?gridx=' + Xpos + '&gridy=' + Ypos;
+    console.log(jd.results[0]);
+    if(jd.status=='ZERO_RESULTS') whe=-1;
+    else{
+      for(var i =0; i<jd.results[0].address_components.length;i++){
+          if(jd.results[0].address_components[i].short_name =='KR'){
+            whe = jd.results[0].geometry.location;
+            calc.mapq(whe.lat,whe.lng);
+            var Xpos = calc.returnX();
+            var Ypos = calc.returnY();
+            console.log(Xpos);
+            console.log(Ypos);
+            wurl = 'http://www.kma.go.kr/wid/queryDFS.jsp?gridx=' + Xpos + '&gridy=' + Ypos;
+            arr[3]=jd.results[0].formatted_address.substring(5,jd.results[0].formatted_address.length);
+            break;
+          }
+          else whe=-1;
+      }
+    }
   });
   setTimeout(()=>{
-    resolve({result:wurl});
-  },1000);
+    if(whe!=-1) resolve({result:wurl});
+    else reject({result:new Error("fail")})
+  },1200);
 });
 
 let B = (wurl)=>new Promise((resolve)=>{
   console.log('B Executed');
-  console.log(wurl);
   request({
     url: wurl,
     method: 'GET'
@@ -85,9 +102,10 @@ let B = (wurl)=>new Promise((resolve)=>{
       data = JSON.stringify(result);
       data = JSON.parse(data);
     })
-    console.log(data.wid.body[0].data[0].temp + 1);
-    ans = (data.wid.body[0].data[0].temp *1) +2;
-    ans +="";
+    arr[0] = '\n ~ '+(data.wid.body[0].data[0].hour*1)%24+'시 예보\n날씨 : '+ data.wid.body[0].data[0].wfKor+'\n온도 : '+data.wid.body[0].data[0].temp+'℃\n강수확률 : '+data.wid.body[0].data[0].pop+'%\n\n';
+    arr[1] = (data.wid.body[0].data[0].hour*1)%24+' ~ '+(data.wid.body[0].data[1].hour*1)%24+'시 예보\n날씨 : '+ data.wid.body[0].data[1].wfKor+'\n온도 : '+data.wid.body[0].data[1].temp+'℃\n강수확률 : '+data.wid.body[0].data[0].pop+'%\n\n';
+    arr[2] = (data.wid.body[0].data[1].hour*1)%24+' ~ '+(data.wid.body[0].data[2].hour*1)%24+'시 예보\n날씨 : '+ data.wid.body[0].data[2].wfKor+'\n온도 : '+data.wid.body[0].data[2].temp+'℃\n강수확률 : '+data.wid.body[0].data[0].pop+'%';
+    ans=arr[3]+arr[0]+arr[1]+arr[2];
   });
   setTimeout(()=>{
     resolve({result:ans});
